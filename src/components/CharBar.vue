00580b692295c4c207d5ee9848b8434ade4bb1ce
Anas Gherfal <anasgherfal@gmail.com>
2023-02-27 10:05:18 +0200

    aded sass

--- src/components/CharBar.vue
+++
@@ -1,7 +0,0 @@
-<template>
-    <p>ALII</p>
-</template>
-
-<script setup lang="ts">
-
-</script>
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,16 +1,11 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 
-import CharBar from '../components/CharBar.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    {
-      path: '/Char',
-      name: 'Char',
-      component: CharBar
-    },
+
     {
       path: '/',
       name: 'home',
