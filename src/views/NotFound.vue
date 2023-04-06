62938188538819adc563fa7253d63ac67882b789
Anas Gherfal <anasgherfal@gmail.com>
2023-04-06 12:36:32 +0200

    added an error page

--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -7,74 +7,82 @@ const goHome =() =>{
 
 }
 
+
+
 </script>
 
 
 <template>
-  <div class="not-found">
-    <div class="not-found__wrapper">
-      <h1 class="not-found__title">404</h1>
-      <p class="not-found__subtitle">Oops! Page not found</p>
-      <p class="not-found__message">The page you're looking for doesn't seem to exist.</p>
-      <button class="not-found__button" @click="goHome">Go back to home</button>
-    </div>
-  </div>
+<div id="main">
+    	<div class="fof">
+        		<h1> 404</h1>
+                <h2>الصفحة غير موجودة!</h2>
+                <h3>يبدو أن الصفحة التي تبحث عنها غير موجودة</h3>
+                <Button class="not-found__button" @click="goHome" label="اذهب للصفحه الرئيسية"></Button>
+    	</div>
+</div>
   </template>
-  
-  
-  <style scoped>
-  .not-found {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    height: 50vh;
-    background-color: #f2f2f2;
-  }
-  
-  .not-found__wrapper {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-    background-color: #fff;
-    padding: 4rem;
-    border-radius: 4px;
-    box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
-  }
-  
-  .not-found__title {
-    font-size: 10rem;
-    font-weight: bold;
-    margin-bottom: 1rem;
-    color: #2b2b2b;
-    letter-spacing: -10px;
-  }
-  
-  .not-found__subtitle {
-    font-size: 3rem;
-    margin-bottom: 2rem;
-    color: #2b2b2b;
-  }
-  
-  .not-found__message {
-    font-size: 2rem;
-    margin-bottom: 2rem;
-    color: #2b2b2b;
-  }
-  
-  .not-found__button {
-    padding: 1rem 2rem;
-    border: none;
-    border-radius: 4px;
-    background-color: #2255ff;
-    color: #fff;
-    font-weight: bold;
-    text-transform: uppercase;
-    transition: background-color 0.3s ease-in-out;
-    box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
-  }
-  
-  .not-found__button:hover {
-    background-color: #183fbb;
-  }
-  </style>
+
+<style scoped>
+/* Center the contents vertically and horizontally */
+*{
+    transition: all 0.6s;
+}
+
+html {
+    height: 100%;
+}
+
+body{
+    font-family: 'Lato', sans-serif;
+    color: #888;
+    margin: 0;
+}
+
+#main{
+    display: table;
+    width: 100%;
+    text-align: center;
+}
+
+.fof{
+	  display: table-cell;
+	  vertical-align: middle;
+}
+
+.fof h1{
+	  font-size: 100px;
+	  display: inline-block;
+	  padding-right: 12px;
+	  animation: type .5s alternate infinite;
+      margin-bottom: 10px;
+
+}
+
+.fof h2{
+    font-size: 50px;
+	  padding-right: 12px;
+      margin-bottom: 10px;
+
+}
+
+.not-found__button {
+  padding: 1rem 2rem;
+  border-radius: 4px;
+  background-color: #2255ff;
+  color: #fff;
+  font-weight: bold;
+  text-transform: uppercase;
+  transition: background-color 0.3s ease-in-out;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
+}
+
+.not-found__button:hover {
+  background-color: #183fbb;
+}
+
+@keyframes type{
+	  from{box-shadow: inset -3px 0px 0px #888;}
+	  to{box-shadow: inset -3px 0px 0px transparent;}
+}
+</style>
