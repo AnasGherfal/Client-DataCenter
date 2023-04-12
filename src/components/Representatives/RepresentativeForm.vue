b9d0a99f73913f023af78589f6214cba76b79d2a
Anas Gherfal <anasgherfal@gmail.com>
2023-04-12 14:44:46 +0200

    added edit rep

---
+++ src/components/Representatives/RepresentativeForm.vue
@@ -0,0 +1,117 @@
+<script setup lang="ts">
+import type { Representatives } from '@/views/Customers/modules/Representatives';
+import { toNumber } from '@vue/shared';
+import useVuelidate from '@vuelidate/core';
+import { helpers, required, email } from '@vuelidate/validators';
+import { computed, reactive, ref, defineEmits, getCurrentInstance } from 'vue';
+import { useRoute } from 'vue-router';
+
+const props = defineProps({
+    representatives: {
+        type: Object as () => Representatives,
+        required: true
+    },
+    submitButtonText: {
+        type: String,
+        required: true
+    },
+    
+})
+
+const representatives = ref(props.representatives)
+
+const instance = getCurrentInstance()
+const submitForm = async () => {
+    const result = await v$.value.$validate();
+
+    if (result) {
+        if (instance) {
+            // Form submission logic here
+            // You can access the form data via formData.value
+            instance.emit('form-submit', representatives.value);
+        }
+
+
+    } else {
+        console.log("empty")
+    }
+}
+
+const rules = computed(() => {
+    return {
+        firstName: { required: helpers.withMessage('الاسم مطلوب', required) },
+        lastName: { required: helpers.withMessage('الاسم مطلوب', required) },
+
+        email: { required: helpers.withMessage('الايميل مطلوب', required), email: helpers.withMessage(' ليس عنوان بريد إلكتروني صالح', email) },
+        phoneNo: { required: helpers.withMessage('رقم الهاتف مطلوب', required) },
+
+    }
+})
+const v$ = useVuelidate(rules, representatives);
+
+
+
+// const displayModal = ref(false);
+// const openModal = () => {
+//     displayModal.value = true;
+// };
+
+
+
+</script>
+<template>
+
+
+        
+
+
+    <form @submit.prevent="submitForm">
+
+        <div class="grid p-fluid">
+            <div class="field col-12 md:col-6 lg:col-4">
+                <span class="p-float-label">
+                    <InputText id="name" type="text" v-model="representatives.firstName" />
+                    <label for="name">الاسم </label>
+                    <error v-for="error in v$.firstName.$errors" :key="error.$uid" class="p-error">{{ error.$message }}</error>
+                </span>
+            </div>
+            <div class="field col-12 md:col-6 lg:col-4">
+                <span class="p-float-label">
+                    <InputText id="name" type="text" v-model="representatives.lastName" />
+                    <label for="name">اللقب </label>
+                    <error v-for="error in v$.lastName.$errors" :key="error.$uid" class="p-error">{{ error.$message }}</error>
+                </span>
+            </div>
+            <div class="field col-12 md:col-6 lg:col-4">
+                <span class="p-float-label ">
+                    <InputText id="email" type="text" v-model="representatives.email" />
+                    <label for="email">البريد الإلكتروني</label>
+                    <error v-for="error in v$.email.$errors" :key="error.$uid" class="p-error">{{ error.$message }}</error>
+                </span>
+            </div>
+            <div class="field col-12 md:col-6 lg:col-4">
+                <span class="p-float-label ">
+                    <InputMask v-model="representatives.phoneNo" mask="+218999999999" />
+                    <label for="inputtext">رقم هاتف </label>
+                    <error v-for="error in v$.phoneNo.$errors" :key="error.$uid" class="p-error">{{ error.$message }}</error>
+                </span>
+            </div>
+            <div class="field col-12 md:col-6 lg:col-4">
+                <span class="p-float-label ">
+                    <InputText id="inputtext" type="text" v-model="representatives.identityType" />
+                    <label for="inputtext">نوع الاثبات</label>
+                </span>
+            </div>
+            <div class="field col-12 md:col-6 lg:col-4">
+                <span class="p-float-label ">
+                    <InputText id="inputtext" v-model="representatives.identityNo" />
+                    <label for="inputtext">رقم الاثبات </label>
+                </span>
+            </div>
+        </div>
+        <Button type="submit" icon="pi pi-check" label="إضافة" />
+    
+
+    </form>
+
+</template>
--- src/views/Customers/Profile Customer/Representatives.vue
+++ src/views/Customers/Profile Customer/Representatives.vue
@@ -1,13 +1,13 @@
 <script setup lang="ts">
-import { computed, reactive, ref } from 'vue';
-import { email, minLength, required, helpers } from "@vuelidate/validators";
-import { useVuelidate } from "@vuelidate/core";
+import { computed, provide, reactive, ref, type Ref } from 'vue';
 import { useToast } from "primevue/usetoast";
 import Dialog from 'primevue/dialog';
 import type { Representatives } from '../modules/Representatives';
 import axios from 'axios';
-import { RouterLink, routerKey, useRoute } from 'vue-router';
+import { useRoute } from 'vue-router';
 import { toNumber } from '@vue/shared';
+import RepresentativeForm from '@/components/Representatives/RepresentativeForm.vue';
+
 
 
 const route = useRoute()
@@ -21,155 +21,76 @@ const userId = computed(() => {
 })
 
 const emit = defineEmits(['getRepresentatives'])
-const representatives: Representatives = reactive({
+const representatives = ref<Representatives>({
     firstName: "",
     lastName: "",
     identityNo: "",
     email: "",
     phoneNo: "",
     identityType: null, //1 personalId 2-authorized 3-representitive
-    customerId: toNumber(userId)
-
-
+    customerId: toNumber(userId.value)
 })
 
-const rules = computed(() => {
-    return {
-        firstName: { required: helpers.withMessage('الاسم مطلوب', required) },
-        lastName: { required: helpers.withMessage('الاسم مطلوب', required) },
-
-        email: { required: helpers.withMessage('الايميل مطلوب', required), email: helpers.withMessage(' ليس عنوان بريد إلكتروني صالح', email) },
-        phoneNo: { required: helpers.withMessage('رقم الهاتف مطلوب', required) },
-
-    }
-})
 
 const toast = useToast();
 
-const v$ = useVuelidate(rules, representatives);
 
-const submitForm = async () => {
-    const result = await v$.value.$validate();
-    if (result) {
-        console.log('1')
-        axios.post("https://localhost:7003/api/Representives", representatives)
-            .then(function (response) {
+const onFormSubmit = async (representative: Representatives) => {
 
-                emit('getRepresentatives')
-                toast.add({ severity: 'success', summary: 'Success Message', detail: 'تمت إضافة باقة', life: 3000 });
-            })
-            .catch(function (error) {
-                console.log(error)
-                toast.add({ severity: 'warn', summary: 'Warn Message', detail: 'هناك مشكلة في عملية الادخال', life: 3000 });
-            })
-        resetForm();
-        displayModal.value = false;
+    axios.post("https://localhost:7003/api/Representives", representative)
+        .then(function (response) {
 
-
-    } else {
-        console.log("empty")
-    }
+            emit('getRepresentatives')
+            toast.add({ severity: 'success', summary: 'Success Message', detail: 'تمت إضافة باقة', life: 3000 });
+        })
+        .catch(function (error) {
+            console.log(error)
+            toast.add({ severity: 'warn', summary: 'Warn Message', detail: 'هناك مشكلة في عملية الادخال', life: 3000 });
+        })
+    displayModal.value = false;
+    resetForm();
 }
-
 
 const resetForm = () => {
-    representatives.firstName = '';
-    representatives.lastName = '';
+    representatives.value.firstName = '';
+    representatives.value.lastName = '';
 
-    representatives.email = '';
-    representatives.phoneNo = '';
-    representatives.identityNo = '';
-    representatives.identityType = null;
+    representatives.value.email = '';
+    representatives.value.phoneNo = '';
+    representatives.value.identityNo = '';
+    representatives.value.identityType = null;
+
 }
-
 const displayModal = ref(false);
-
 const openModal = () => {
     displayModal.value = true;
+
 };
-const closeModal = () => {
-    displayModal.value = false;
-}
+
 </script>
 
 
 <template >
     <div>
-        <Dialog header="اضافة مُخول" contentStyle="height: 280px; padding: 20px;" v-model:visible="displayModal"
-            :breakpoints="{ '960px': '75vw', '640px': '90vw' }" :style="{ width: '60vw' }" :modal="true">
 
-
-            <template #default>
-                <form @submit.prevent="submitForm">
-
-                    <div class="grid p-fluid ">
-                        <div class="field col-12 md:col-6 lg:col-4">
-                            <span class="p-float-label">
-                                <InputText id="name" type="text" v-model="representatives.firstName" />
-                                <label for="name">الاسم </label>
-                                <error v-for="error in v$.firstName.$errors" :key="error.$uid" class="p-error">{{
-                                    error.$message }}</error>
-                            </span>
-
-                        </div>
-                        <div class="field col-12 md:col-6 lg:col-4">
-                            <span class="p-float-label">
-                                <InputText id="name" type="text" v-model="representatives.lastName" />
-                                <label for="name">اللقب </label>
-                                <error v-for="error in v$.lastName.$errors" :key="error.$uid" class="p-error">{{
-                                    error.$message }}</error>
-                            </span>
-
-                        </div>
-                        <div class="field col-12 md:col-6 lg:col-4">
-                            <span class="p-float-label ">
-                                <InputText id="email" type="text" v-model="representatives.email" />
-                                <label for="email">البريد الإلكتروني</label>
-                                <error v-for="error in v$.email.$errors" :key="error.$uid" class="p-error">{{ error.$message
-                                }}</error>
-
-                            </span>
-                        </div>
-
-                        <div class="field col-12 md:col-6 lg:col-4">
-                            <span class="p-float-label ">
-                                <InputMask v-model="representatives.phoneNo" mask="+218999999999" />
-                                <label for="inputtext">رقم هاتف </label>
-                                <error v-for="error in v$.phoneNo.$errors" :key="error.$uid" class="p-error">{{
-                                    error.$message }}</error>
-
-                            </span>
-                        </div>
-
-                        <div class="field col-12 md:col-6 lg:col-4">
-                            <span class="p-float-label ">
-                                <InputText id="inputtext" type="text" v-model="representatives.identityType" />
-                                <label for="inputtext">نوع الاثبات</label>
-
-                            </span>
-                        </div>
-                        <div class="field col-12 md:col-6 lg:col-4">
-                            <span class="p-float-label ">
-                                <InputText id="inputtext" v-model="representatives.identityNo" />
-                                <label for="inputtext">رقم الاثبات </label>
-                            </span>
-                        </div>
-
-
-
-                    </div>
-                    <Button @click="submitForm" icon="pi pi-check" label="إضافة" />
-                    <Button @click="resetForm" icon="fa-solid fa-delete-left" label="مسح" class="p-button-danger"
-                        style="margin-right: .5rem;" />
-
-                </form>
-            </template>
-
-
-        </Dialog>
         <Button @click="openModal" class=" p-button-primary mb-4">
             اضافة مُخول
         </Button>
+        <Dialog header="اضافة مُخول" contentStyle="max-height: 80vh; width: 75vw; padding: 20px;"
+            v-model:visible="displayModal" :breakpoints="{ '960px': '75vw', '640px': '90vw' }" :modal="true">
+
+            <template #default>
+
+
+                <RepresentativeForm @form-submit="onFormSubmit" :representatives="representatives"
+                    :submitButtonText="'add'">
+
+                </RepresentativeForm>
+
+            </template>
+
+        </Dialog>
+
     </div>
 </template>
 
@@ -189,10 +110,5 @@ const closeModal = () => {
     align-items: end;
     align-content: end;
 
-}
-
-error {
-    font-size: 12px;
-    font-weight: bold;
 }
 </style>
